d68b5ba4453e00427e4bb30757f3b45b35169a9c
Taimoor <taimoorsaleem2005@gmail.com>
2026-02-06 18:59:41 +0500

    Patient Action Chart

--- src/Doctor/DoctorAnalytics.jsx
+++ src/Doctor/DoctorAnalytics.jsx
@@ -11,22 +11,39 @@ import {
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoctorAnalytics = () => {
-    let patients = getLocalStorage()
-    let appointments = getAppointment()
+  let patients = getLocalStorage()
+  let appointments = getAppointment()
 
-      const data = {
-    labels: ["Recovered", "Active", "Critical"],
+  const countActions = () => {
+
+    let actions = patients.map((patient)=>{
+      return patient.action
+    })
+    const countMap = {
+      Admitted: 0,
+      Recovering: 0,
+      Critical: 0,
+      Discharged: 0
+    }
+
+    for (let i = 0; i < actions.length; i++) {
+      countMap[actions[i]]++
+    }
+
+    return Object.values(countMap)
+  }
+
+  const data = {
+    labels: ["Admitted","Recovering", "Critical","Discharged"],
     datasets: [
       {
         label: "Patients",
-        // data: patients.map((patient)=>{
-        //     if(patient.action)
-        // }),
-        data:[20,30,50],
+        data: countActions(),
         backgroundColor: [
-          "#0FB9B1", // green
-          "#3b82f6", // blue
-          "#ef4444", // red
+          "#1F2D3D", // green
+          "#0FB9B1", // red
+          "#ef4444",
+          "#3BAFDA"
         ],
         borderWidth: 1,
       },
@@ -52,32 +69,7 @@ const DoctorAnalytics = () => {
       </div>
     </div>
   );
-    
-    // return (
-        // <>
-        {/* <div className="bg-white shadow-sm w-full rounded-lg p-8 max-w-[1200px] mx-auto">
-            <h1 className="text-2xl font-bold text-text-heading">Analytics Overview</h1>
-            <div className="relative w-full grid grid-cols-1 sm:grid-cols-3 justify-items-center mt-10">
-                <svg width="200" height="200" className="scale-125">
-                    <circle r="70" cx="100" cy="100" fill="transparent" stroke="lightgrey" stroke-width="1.5rem" stroke-dasharray="439.8" stroke-dashoffset="0"></circle>
-                    <circle r="70" cx="100" cy="100" fill="transparent" stroke="#0FB9B1" stroke-width="1.5rem" stroke-dasharray="439.8" stroke-dashoffset={3000-patients.length}></circle>
-                </svg>
-                <svg width="200" height="200" className="scale-125">
-                    <circle r="70" cx="100" cy="100" fill="transparent" stroke="lightgrey" stroke-width="1.5rem" stroke-dasharray="439.8" stroke-dashoffset="0"></circle>
-                    <circle r="70" cx="100" cy="100" fill="transparent" stroke="#3BAFDA" stroke-width="1.5rem" stroke-dasharray="439.8" stroke-dashoffset={3000-appointments.length}></circle>
-                </svg>
-                <svg width="200" height="200" className="scale-125">
-                    <circle r="70" cx="100" cy="100" fill="transparent" stroke="lightgrey" stroke-width="1.5rem" stroke-dasharray="439.8" stroke-dashoffset="0"></circle>
-                    <circle r="70" cx="100" cy="100" fill="transparent" stroke="#1F2D3D" stroke-width="1.5rem" stroke-dasharray="439.8" stroke-dashoffset="100"></circle>
-                </svg>
-            </div>
-        </div> */}
 
-        
-
-        {/* </> */}
-
-    // )
 };
 
 
